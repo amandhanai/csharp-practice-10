66dd8573e221caaf612a927313ce83e796a600f1
Aman <Aman@gmail.com>
2021-02-10 18:26:18 +0530

    Csharppracticefiles

---
+++ csharp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace csharp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("PLease Enter your First Name  ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("PLease Enter your Middle Name  ");
+            string MiddleName  = Console.ReadLine();
+            Console.WriteLine("PLease Enter your Last Name  ");
+            string LastName = Console.ReadLine();
+            
+            // Console.WriteLine("Hello {0}", Username);
+            Console.WriteLine("Hello {0},{1},{2} ",  FirstName ,MiddleName,LastName );
+            
+            Console.ReadLine();
+             
+
+        }
+    }
+}
